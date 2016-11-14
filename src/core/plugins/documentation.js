cab2b7349d5c1251d4b816140782caae40199e1f
Fernando Cagale <fernando.b.cagale@gmail.com>
2016-11-14 13:25:37 -0200

    alter plugin documentation

--- src/core/plugins/documentation.js
+++ src/core/plugins/documentation.js
@@ -12,7 +12,7 @@ exports.register = (server, options, next) => {
       'version': Pack.version,
       'termsOfService': 'https://github.com/glennjones/hapi-swagger/',
       'contact': {
-        'email': 'contact@nestormagalhaes.cc'
+        'email': 'fernando.b.cagale@gmail.com'
       },
       'license': {
         'name': 'MIT',
